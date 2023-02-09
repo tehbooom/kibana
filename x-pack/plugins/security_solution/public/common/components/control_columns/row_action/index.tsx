b4f033f493baff9eb3afe6f3a4e6a3497201afeb
PhilippeOberti <philippe.oberti@elastic.co>
2023-02-09 14:06:17 -0600

    [Security Solution ] security solution expanded flyout
    - feature flag in security_solution plugin
    - new kbn-expandable-flyout package
    - skeleton of the expanded flyout with left, right and preview panels

--- x-pack/plugins/security_solution/public/common/components/control_columns/row_action/index.tsx
+++ x-pack/plugins/security_solution/public/common/components/control_columns/row_action/index.tsx
@@ -8,6 +8,7 @@
 import type { EuiDataGridCellValueElementProps } from '@elastic/eui';
 import React, { useCallback, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
+import { useExpandableFlyoutContext } from '@kbn/expandable-flyout';
 import type {
   SetEventsDeleted,
   SetEventsLoading,
@@ -19,6 +20,7 @@ import { getMappedNonEcsValue } from '../../../../timelines/components/timeline/
 import type { TimelineItem, TimelineNonEcsData } from '../../../../../common/search_strategy';
 import type { ColumnHeaderOptions, OnRowSelected } from '../../../../../common/types/timeline';
 import { dataTableActions } from '../../../store/data_table';
+import { useIsExperimentalFeatureEnabled } from '../../../hooks/use_experimental_features';
 
 type Props = EuiDataGridCellValueElementProps & {
   columnHeaders: ColumnHeaderOptions[];
@@ -70,7 +72,10 @@ const RowActionComponent = ({
     return rowData ?? {};
   }, [data, pageRowIndex]);
 
+  const { openFlyout } = useExpandableFlyoutContext();
+
   const dispatch = useDispatch();
+  const isSecurityFlyoutEnabled = useIsExperimentalFeatureEnabled('securityFlyoutEnabled');
 
   const columnValues = useMemo(
     () =>
@@ -96,14 +101,18 @@ const RowActionComponent = ({
       },
     };
 
-    dispatch(
-      dataTableActions.toggleDetailPanel({
-        ...updatedExpandedDetail,
-        tabType,
-        id: tableId,
-      })
-    );
-  }, [dispatch, eventId, indexName, tabType, tableId]);
+    if (isSecurityFlyoutEnabled) {
+      openFlyout({});
+    } else {
+      dispatch(
+        dataTableActions.toggleDetailPanel({
+          ...updatedExpandedDetail,
+          tabType,
+          id: tableId,
+        })
+      );
+    }
+  }, [dispatch, eventId, indexName, isSecurityFlyoutEnabled, openFlyout, tabType, tableId]);
 
   const Action = controlColumn.rowCellRender;
 
